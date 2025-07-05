3c918f20531fa874014bab7202b71a6c9cfc6b9a
Nnnastie <148958030+Nnnastie@users.noreply.github.com>
2025-07-05 20:24:04 +0200

    syhrimp
    shrimp

--- template/index.js
+++ template/index.js
@@ -1,55 +1,59 @@
 // Classifier Variable
 let classifier;
 // Model URL
-// HERE
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/6eL2SJyis/';
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/M18U0FNUa/';
 
 // Video
 let video;
-let flippedVideo;
 // To store the classification
 let label = '';
 
-// Load the model first
+// Images for good and bad posture
+let goodImg, badImg;
+
 function preload() {
-	classifier = ml5.imageClassifier(imageModelURL + 'model.json');
-	console.log(classifier);
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  
+  // Load images (make sure good.png and bad.png are in the same folder)
+  goodImg = loadImage('shrimp.png'); 
+  badImg = loadImage('shrimp2.png'); 
 }
 
 function setup() {
-	createCanvas(320, 260);
-	// Create the video
-	video = createCapture(VIDEO);
-	video.size(320, 240);
-	video.hide();
-
-	// Start classifying
-	classifyVideo();
+  createCanvas(320, 480); // Top part for the image, bottom part for video
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.hide();
+  classifyVideo();
 }
 
 function draw() {
-	background(0);
-	// Draw the video
-	image(video, 0, 0);
+  background(0);
+  
+  // Show the corresponding image on top
+  if (label === 'good posture') {
+    image(goodImg, 0, 0, 320, 240);
+  } else if (label === 'bad posture') {
+    image(badImg, 0, 0, 320, 240);
+  }
 
-	// Draw the label
-	fill(255);
-	textSize(16);
-	textAlign(CENTER);
-	text(label, width / 2, height - 4);
+  // Show the video below the image
+  image(video, 0, 240, 320, 240);
+
+  // Show the label as text below the video
+  fill(255);
+  textSize(14);
+  textAlign(CENTER);
+  text(label, width / 2, height - 10);
 }
 
-// Get a prediction for the current video frame
 function classifyVideo() {
-	classifier.classify(video, gotResult);
+  classifier.classify(video, gotResult);
 }
 
-// When we get a result
 function gotResult(results) {
-	console.log(results);
-	// The results are in an array ordered by confidence.
-	// console.log(results[0]);
-	label = results[0].label;
-	// Classifiy again!
-	classifyVideo();
+  if (results && results[0]) {
+    label = results[0].label;
+  }
+  classifyVideo();
 }
